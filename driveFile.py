01e8f3ff9660e845dec91c87494fda1120e2ba9f
Marc Donner <marc.donner@gmail.com>
2018-04-21 12:37:32 -0400

    Start porting the Drive access scaffolding in.  Keep the main framework working, please.

--- driveFile.py
+++ driveFile.py
@@ -23,7 +23,47 @@ from oauth2client.file import Storage
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-# class DriveFile(object):
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+CLIENT_SECRET_FILE = '.client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+class DriveFile(object):
+    """Class to provide cached access to Google Drive object metadata."""
+
+    def __init__(self):
+        self.fileData = {}
+        self.credentials = get_credentials()
+        self.service = discovery.build('drive', 'v3', http=http)
 
 def main():
 
